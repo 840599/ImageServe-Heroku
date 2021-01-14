23f0335134652aa87913d4af6125a432188be58a
840599 <71902057+840599@users.noreply.github.com>
2021-01-13 18:42:23 -0600

    Update config.php

--- protected/config/config.php
+++ protected/config/config.php
@@ -13,7 +13,7 @@
 /* https://github.com/aerouk/imageserve/wiki/Configuration */
 
 define('RAW_IMAGE', true);
-define('RAW_IMAGE_LINK', false);
+define('RAW_IMAGE_LINK', true);
 define('IMAGE_EXTENSION', true);
 
 define('TWITTER_CARDS', false);
@@ -23,5 +23,5 @@ define('TWITTER_CARDS', false);
 define('IMAGESERVE_DIR', '');
 define('TWITTER_HANDLE', '@aerouk_');
 
-define('APP_NAME', 'ImageServe-Heroku');
-define('PASSKEY', getenv('PASSKEY'));
+define('APP_NAME', 'h-image');
+define('PASSKEY', getenv('copernicus'));
